4fe770cebd73ac4d5b849bfa29a4469afd018781
Henry Sztul <henry@sztul.com>
2012-02-29 15:27:58 -0500

    added user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
 
 
 end
