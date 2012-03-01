a0c120e613c679e38ae75e22553ba71e37f14277
Henry Sztul <henry@sztul.com>
2012-03-01 09:16:04 -0500

    added roll controller

---
+++ app/controllers/roll_controller.rb
@@ -0,0 +1,20 @@
+class RollController < ApplicationController  
+
+  def show
+    @roll = Roll.find(params[:id])
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+
+
+end
