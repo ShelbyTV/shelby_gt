65e5c8190571aedfc83fdfc3068d1c9b2e10ca27
Henry Sztul <henry@sztul.com>
2012-03-13 16:56:53 -0400

    changed ref to nos in exception notification

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Nos::Application.config.middleware.use ExceptionNotifier,
+  ShelbyGt::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[GT Backend Error] ",
     :sender_address => %{"ShelbyGT Notifier" <whatever-noreply@shelby.com>},
     :exception_recipients => %w{webapp-errors@shelby.tv}
