f4e7f023964865fe8fbd3833d96f68fedd886c73
Henry Sztul <henry@sztul.com>
2012-03-13 17:36:59 -0400

    added passenger restarting to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,25 @@ set :application, "shelby_gt"
 default_run_options[:pty] = true
 
 #############################################################
+#	Passenger
+#############################################################
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+    #run 'curl -sL -w "I just tapped %{url_effective}: %{http_code}\\n" "http://shelby.tv" -o /dev/null'
+  end
+end
+
+namespace :deploy do
+  desc "Restart passenger"
+  task :restart do
+    passenger.restart
+  end
+end
+
+#############################################################
 #	Bundler
 #############################################################
 require "bundler/capistrano"
