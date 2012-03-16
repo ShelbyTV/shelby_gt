dc6ce93e89191067fff4a7445a60491088715c0e
Henry Sztul <henry@sztul.com>
2012-03-16 14:46:16 -0400

    added a key to allow us to test api without signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :allow_faux_authentication!
+  
   respond_to :json
+  
+  private
+
+    def allow_faux_authentication!
+      if params[:key] == "1234567890"
+        user = User.find_by_nickname('sztul')
+        sign_in(:user, user)
+      end
+    end
   
 end
