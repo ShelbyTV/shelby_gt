4f02c461014c27fc750798e5ff2a196025c1129f
Henry Sztul <henry@sztul.com>
2012-03-16 10:37:24 -0400

    turning on exception notification and removed ref to favion in tmplt

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,8 +1,6 @@
-=begin
 if Rails.env == "production"
   ShelbyGt::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[GT Backend Error] ",
     :sender_address => %{"ShelbyGT Notifier" <whatever-noreply@shelby.com>},
     :exception_recipients => %w{webapp-errors@shelby.tv}
 end
-=end
