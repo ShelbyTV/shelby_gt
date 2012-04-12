277d52e2215b98001d929e138091b722600ab3d9
Henry Sztul <henry@sztul.com>
2012-04-12 11:58:17 -0400

    checking if user has email address

--- lib/gt/notification_manager.rb
+++ lib/gt/notification_manager.rb
@@ -6,7 +6,7 @@ module GT
       raise ArgumentError, "must supply valid frame" unless frame.is_a?(Frame) and !frame.blank?
       
       # don't email the creator if they are the upvoting user!
-      return if user.id == frame.creator_id
+      return unless (user.id != frame.creator_id) and user.primary_email
       
       NotificationMailer.upvote_notification(user, frame.creator, frame).deliver
     end
@@ -15,10 +15,12 @@ module GT
       raise ArgumentError, "must supply valid user" unless user.is_a?(User) and !user.blank?
       raise ArgumentError, "must supply valid conversation" unless conversation.is_a?(Conversation) and !conversation.blank?
       
-      # cant email anyone if the conversation initiator is not a shelby user :)
-      return if conversation.messages.first.user.faux == User::FAUX_STATUS[:true]
+      first_message = conversation.messages.first
       
-      NotificationMailer.comment_notification(user, frame.creator, conversation).deliver
+      # cant email anyone if we dont have their email address :)
+      return unless first_message and first_message.user and first_message.user.primary_email
+      
+      NotificationMailer.comment_notification(user, first_message.user, conversation).deliver
     end
     
   end
