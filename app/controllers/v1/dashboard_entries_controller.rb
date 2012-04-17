751b6ad5fc6dff5f755ca74bffa554a886714204
Henry Sztul <henry@sztul.com>
2012-04-17 09:57:40 -0400

    added compact method on eager loading arrays and updated template again

--- app/controllers/v1/dashboard_entries_controller.rb
+++ app/controllers/v1/dashboard_entries_controller.rb
@@ -36,14 +36,14 @@ class V1::DashboardEntriesController < ApplicationController
 
         #########
         # solving the N+1 problem with loading all children of a dashboard_entry
-        @entries_frame_ids = @entries.map {|e| e.frame_id }.uniq
+        @entries_frame_ids = @entries.map {|e| e.frame_id }.compact.uniq
                 
         @frames = Frame.find(@entries_frame_ids)
 
-        @entries_roll_ids = @frames.map {|f| f.roll_id }.uniq
-        @entries_creator_ids = @frames.map {|f| f.creator_id }.uniq        
-        @entries_conversation_ids = @frames.map {|f| f.conversation_id }.uniq
-        @entries_video_ids = @frames.map {|f| f.video_id }.uniq
+        @entries_roll_ids = @frames.map {|f| f.roll_id }.compact.uniq
+        @entries_creator_ids = @frames.map {|f| f.creator_id }.compact.uniq        
+        @entries_conversation_ids = @frames.map {|f| f.conversation_id }.compact.uniq
+        @entries_video_ids = @frames.map {|f| f.video_id }.compact.uniq
 
         @rolls = Roll.find(@entries_roll_ids)
         @creators = User.find(@entries_user_ids)        
--- app/views/v1/dashboard_entries/show_with_children.json.rabl
+++ app/views/v1/dashboard_entries/show_with_children.json.rabl
@@ -22,7 +22,11 @@ child :frame => "frame" do
 		attributes :id, :public
 
 		child :messages => 'messages' do
-			extends 'v1/messages/show'
+			attributes :id, :nickname, :realname, :user_image_url, :text, :origin_network, :origin_id, :origin_user_id, :user_id, :public
+
+			code :created_at do |c|
+				time_ago_in_words(c.created_at) + ' ago' if c.created_at
+			end
 		end
 	end
 	
