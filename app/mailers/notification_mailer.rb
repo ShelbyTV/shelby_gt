7801d1f2b07274b5b661f9955de442258774bde3
Henry Sztul <henry@sztul.com>
2012-04-20 12:48:43 -0400

    working on comment notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,15 +2,18 @@ class NotificationMailer < ActionMailer::Base
   include SendGrid
   sendgrid_enable   :ganalytics, :opentrack, :clicktrack
 
-  def comment_notification(user_to, user_from, conversation, message)
+  def comment_notification(user_to, user_from, frame, message)
     sendgrid_category Settings::Email.comment_notification["category"]
     if user_to.primary_email
       @user_to = user_to
       @user_from = user_from
-      @conversation = conversation
       @message = message
-      #TODO: needs frame
+      @frame = frame
+      
+      # only send email if we find the frame
+      @permalink = "#{Settings::Email.web_url_base}/roll/#{@frame.roll.id}/frame/#{@frame.id}"
       mail :from => Settings::Email.notification_sender, :to => user_to.primary_email, :subject => Settings::Email.comment_notification['subject']
+      
     end
   end
 
@@ -21,6 +24,7 @@ class NotificationMailer < ActionMailer::Base
       @user_from = user_from
       @frame = frame
       @permalink = "#{Settings::Email.web_url_base}/roll/#{@frame.roll.id}/frame/#{@frame.id}"
+      
       mail :from => "notifications@shelby.tv", :to => user_to.primary_email, :subject => Settings::Email.upvote_notification['subject']
     end
   end
--- lib/gt/notification_manager.rb
+++ lib/gt/notification_manager.rb
@@ -11,31 +11,32 @@ module GT
       return if (user.id == frame.creator_id) or !user.primary_email or (user.primary_email == "")
       
       # only sending notifications for a select few for now
-      #return unless ["henrysztul"].include?(frame.creator.nickname)
+      return unless ["henry", "spinosa", "reece", "mmatyus", "chris"].include?(frame.creator.nickname)
       
       NotificationMailer.upvote_notification(frame.creator, user, frame).deliver
     end
     
-    def self.check_and_send_comment_notification(user, c, message)
-      raise ArgumentError, "must supply valid user" unless user.is_a?(User) and !user.blank?
+    def self.check_and_send_comment_notification(u, c, new_message)
+      raise ArgumentError, "must supply valid user" unless u.is_a?(User) and !u.blank?
       raise ArgumentError, "must supply valid conversation" unless c.is_a?(Conversation) and !c.blank?
-      raise ArgumentError, "must supply valid message" unless message.is_a?(Message) and !message.blank?
+      raise ArgumentError, "must supply valid message" unless new_message.is_a?(Message) and !new_message.blank?
       
-      first_message = c.messages.first
+      #return if Rails.env != "production"
       
-      c.messages.each do |m|
-        # stop if the message user is the current user
-        break if m.user == user
-        
+      # stop if the message user is the user taking the action
+      return if new_message.user == u
+
+      # stop if we don't get the frame (otherwise we won't have a permalink for the email)      
+      #TODO: This is probably not the *best* way to get the conversations frame, so find a  better way...
+      return unless frame = Frame.where(:conversation_id => c.id).first
+      
+      c.messages.each do |old_message|
         # cant email anyone if we dont have their email address :)
-        break unless m and m.user and m.user.primary_email
+        break unless old_message.user and old_message.user.primary_email
         
-        if Rails.env == "production"
-          # only sending notifications for a select few for now
-          break unless ["henrysztul"].include?(m.user.nickname)
-        end
-        
-        NotificationMailer.comment_notification(first_message.user, m.user, c, m).deliver
+        break unless ["henry", "spinosa", "reece", "mmatyus", "chris"].include?(old_message.user.nickname)
+                
+        NotificationMailer.comment_notification(old_message.user, new_message.user, frame, new_message).deliver
       end
     end
   end
