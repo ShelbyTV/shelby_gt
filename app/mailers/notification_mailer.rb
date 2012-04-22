7da23fbee68bddfb9b3bf6901887cd0b5a3afe91
Henry Sztul <henry@sztul.com>
2012-04-22 14:43:22 -0400

    building reroll notification

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -25,4 +25,21 @@ class NotificationMailer < ActionMailer::Base
     end
   end
 
+  def reroll_notification(old_frame, new_frame)
+    sendgrid_category Settings::Email.reroll_notification["category"]
+
+    @user_to = old_frame.creator
+    @user_from = new_frame.creator
+
+    if @user_to.primary_email
+      @old_frame = old_frame
+      @new_frame = new_frame
+      @new_frame_permalink = "#{Settings::Email.web_url_base}/roll/#{@new_frame.roll.id}/frame/#{@new_frame.id}"
+      @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_from.id}/personal_roll"
+      
+      mail :from => "Shelby.tv <#{Settings::Email.notification_sender}>", :to => @user_to.primary_email, :subject => Settings::Email.reroll_notification['subject']
+    end
+  end
+
+
 end
--- lib/gt/notification_manager.rb
+++ lib/gt/notification_manager.rb
@@ -4,10 +4,10 @@ module GT
     def self.check_and_send_upvote_notification(user, frame)
       raise ArgumentError, "must supply valid user" unless user.is_a?(User) and !user.blank?
       raise ArgumentError, "must supply valid frame" unless frame.is_a?(Frame) and !frame.blank?
+      # don't email the creator if they are the upvoting user or they dont have an email address!
+      return if (user.id == frame.creator_id) or !user.primary_email or (user.primary_email == "")
       
-      # don't email the creator if they are the upvoting user!
-      #return unless (user.id != frame.creator_id) and user.primary_email
-      
+      # Temp: for now only send emails to us
       if Rails.env == "production"
         # only sending notifications for a select few for now
         return unless ["henrysztul"].include?(frame.creator.nickname)
@@ -15,18 +15,36 @@ module GT
       
       NotificationMailer.upvote_notification(frame.creator, user, frame).deliver
     end
+
+    def self.check_and_send_reroll_notification(old_frame, new_frame)
+      raise ArgumentError, "must supply valid new frame" unless new_frame.is_a?(Frame) and !new_frame.blank?
+      raise ArgumentError, "must supply valid old frame" unless old_frame.is_a?(Frame) and !old_frame.blank?
+      
+      # don't email the creator if they are the upvoting user or they dont have an email address!
+      user = new_frame.creator
+      return if (new_frame.creator_id == old_frame.creator_id) or !user.primary_email or (user.primary_email == "")
+      
+      # Temp: for now only send emails to us
+      if Rails.env == "production"
+        return unless ["henry", "spinosa", "reece", "mmatyus", "chris"].include?(old_frame.creator.nickname)
+      end
+      
+      NotificationMailer.reroll_notification(new_frame, old_frame).deliver
+    end
     
     def self.check_and_send_comment_notification(user, c, message)
       raise ArgumentError, "must supply valid user" unless user.is_a?(User) and !user.blank?
       raise ArgumentError, "must supply valid conversation" unless c.is_a?(Conversation) and !c.blank?
       raise ArgumentError, "must supply valid message" unless message.is_a?(Message) and !message.blank?
       
-      first_message = c.messages.first
+      # stop if the message user is the user taking the action
+      return if new_message.user == u
+
+      # stop if we don't get the frame (otherwise we won't have a permalink for the email)      
+      #TODO: This is probably not the *best* way to get the conversations frame, so find a  better way...
+      return unless frame = Frame.where(:conversation_id => c.id).first
       
-      c.messages.each do |m|
-        # stop if the message user is the current user
-        break if m.user == user
-        
+      c.messages.each do |old_message|
         # cant email anyone if we dont have their email address :)
         break unless m and m.user and m.user.primary_email
         
