61bc48d1eff14baa92d5a622bb9f129b84769a88
Henry Sztul <henry@sztul.com>
2012-04-23 09:58:12 -0400

    working on tests

--- spec/lib/gt/unit/notification_manager_spec.rb
+++ spec/lib/gt/unit/notification_manager_spec.rb
@@ -66,4 +66,38 @@ describe GT::NotificationManager do
     end
   end
 
+  describe "reroll notifications" do
+    before(:all) do
+      @old_user = Factory.create(:user)
+      @new_user = Factory.create(:user)
+      @old_roll = Factory.create(:roll, :creator => @old_user)
+      @new_roll = Factory.create(:roll, :creator => @new_user)
+      @video = Factory.create(:video)
+      @old_frame = Factory.create(:frame, :creator => @old_user, :video => @video, :roll => @old_roll)
+      @new_frame = Factory.create(:frame, :creator => @new_user, :video => @video, :roll => @new_roll)
+    end
+    
+    it "should should queue email to deliver" do
+      lambda {
+        GT::NotificationManager.check_and_send_reroll_notification(@old_frame, @new_frame)
+      }.should change(ActionMailer::Base.deliveries,:size).by(1)
+    end
+    
+    it "should return nil if user is creator of the frame" do
+      @frame.creator = @user; @frame.save
+      r = GT::NotificationManager.check_and_send_upvote_notification(@user, @frame)
+      #r.should eq(nil)
+    end
+    
+    it "should raise error with bad frame or user" do
+      lambda {
+        GT::NotificationManager.check_and_send_upvote_notification(@user) 
+      }.should raise_error(ArgumentError)
+      
+      lambda { 
+        GT::NotificationManager.check_and_send_upvote_notification(@frame, @frame) 
+      }.should raise_error(ArgumentError)
+    end
+  end
+
 end
