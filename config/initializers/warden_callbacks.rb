4552967d4d0ef305b6d2d68d22e6c15453e7217c
Henry Sztul <henry@sztul.com>
2012-04-23 20:46:56 -0400

    using warden callbacks

--- config/initializers/warden_callbacks.rb
+++ config/initializers/warden_callbacks.rb
@@ -1,17 +1,15 @@
 Warden::Manager.after_set_user do |user, auth, opts|
   auth.cookies[:_shelby_gt_common] = {
-    :value => {:authenticated_user_id => user.id},
+    :value => "authenticated_user_id=#{user.id.to_s}",
     :expires => 1.week.from_now,
     :domain => '.shelby.tv'
   }
-  Rails.logger.info("after_set_user callback: #{auth.cookies.inspect}}")
 end
 
 Warden::Manager.before_logout do |user, auth, opts|
   auth.cookies[:_shelby_gt_common] = {
-    :value => {:authenticated_user_id => nil},
+    :value => "authenticated_user_id=nil",
     :expires => 1.week.from_now,
     :domain => '.shelby.tv'
   }
-  Rails.logger.info("after_set_user callback: auth: #{auth.cookies.inspect}")
 end
