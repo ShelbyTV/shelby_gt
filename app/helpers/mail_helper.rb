db8a4ab3aabf60db8519c26c74c9c7dad7d76a1a
Dan Spinosa <spinosa@gmail.com>
2012-04-24 15:39:08 -0400

    first, decent crack at auto-reponse email

---
+++ app/helpers/mail_helper.rb
@@ -0,0 +1,18 @@
+module MailHelper
+  
+  def standard_html_email_wrapper
+    <<-END_HTML
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+      <html xmlns="http://www.w3.org/1999/xhtml">
+      <head>
+      	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
+      <body class="shelby">
+        #{yield}
+      </body>
+      </html>
+    END_HTML
+  end
+  
+end
