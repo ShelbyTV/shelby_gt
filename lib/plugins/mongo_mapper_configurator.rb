5276cc3b992893548501fcbd8ce8426b3d619805
Dan Spinosa <spinosa@gmail.com>
2012-04-26 09:04:38 -0400

    mongo connection pooling

--- lib/plugins/mongo_mapper_configurator.rb
+++ lib/plugins/mongo_mapper_configurator.rb
@@ -5,9 +5,9 @@ module Plugins
     module ClassMethods
       def configure_mongomapper(settings)
         if settings['db_hosts']
-          connection Mongo::ReplSetConnection.new( settings.db_hosts, settings.db_options )
+          connection Mongo::ReplSetConnection.new( settings.db_hosts, settings.db_options.merge(Settings::Mongo.db_options) )
         else
-          connection Mongo::Connection.new(settings.db_host, settings.db_port, settings.db_options)
+          connection Mongo::Connection.new(settings.db_host, settings.db_port, settings.db_options.merge(Settings::Mongo.db_options) )
         end
         set_database_name settings.db_name
         database.authenticate(settings.db_username, settings.db_password) if settings['db_username'] && settings['db_password']
