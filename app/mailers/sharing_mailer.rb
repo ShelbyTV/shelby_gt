062931c167c08d3e79a9a20e840e3ad1ca2e4a06
Henry Sztul <henry@sztul.com>
2012-04-28 21:12:59 -0400

    updated links in emails, created a user permalink to their personal roll and more

--- app/mailers/sharing_mailer.rb
+++ app/mailers/sharing_mailer.rb
@@ -3,20 +3,22 @@ class SharingMailer < ActionMailer::Base
   sendgrid_category Settings::Email.share_frame["category"]
   sendgrid_enable   :ganalytics, :opentrack, :clicktrack
 
-  def share_frame(user, email_from, email_to, message, frame)
-    @user = user
+  def share_frame(user_from, email_from, email_to, message, frame)
+    @user_from = user_from
     @email_to = email_to
     @message = message if message
     @frame = frame if frame
     mail :from => email_from, :to => email_to, :subject => Settings::Email.share_frame['subject']
   end
 
-  def share_roll(user, email_from, email_to, message, roll)
-    @user = user
+=begin
+  def share_roll(user_from, email_from, email_to, message, roll)
+    @user_from = user_from
     @email_to = email_to
     @message = message if message
     @frame = roll if roll
     mail :from => email_from, :to => email_to, :subject => Settings::Email.share_frame['subject']
   end
+=end
 
 end
