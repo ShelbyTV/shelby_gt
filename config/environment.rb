bc8dd52d21d21e0fad1bf6ba5f6981e1bddc87bc
Henry Sztul <henry@sztul.com>
2012-04-29 13:35:40 -0400

    putting environment settings back to normal. my isp doesnt allow port 25 :(

--- config/environment.rb
+++ config/environment.rb
@@ -8,12 +8,11 @@ ShelbyGt::Application.initialize!
 # Email Setup via Sendgrid:
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
-  :port => 587,
+  :port => 25,
   :domain => "shelby.tv",
   :authentication => :plain,
   :user_name => Settings::Sendgrid.username,
-  :password => Settings::Sendgrid.password,
-  :enable_starttls_auto => true
+  :password => Settings::Sendgrid.password
 }
 # FYI: if port 25 blocked by isp use:
 # port 587 with :enable_starttls_auto => true
