7f56c458e8f68242e2cd1c0e779417db115a63cd
Henry Sztul <henry@sztul.com>
2012-05-02 16:45:40 -0400

    turning on join roll notifications

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -57,6 +57,9 @@ class Roll
   
     self.following_users << FollowingUser.new(:user => u)
     u.roll_followings << RollFollowing.new(:roll => self)
+
+    # send email notification in a non-blocking manor
+    EM.next_tick { GT::NotificationManager.check_and_send_join_roll_notification(u, self) }
     
     GT::UserActionManager.follow_roll!(u.id, self.id) if u.save and self.save
   end
