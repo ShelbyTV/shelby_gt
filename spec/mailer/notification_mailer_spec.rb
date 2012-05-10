920ab2adbfe0299031046e61b0226a999ac439f7
Dan Spinosa <spinosa@gmail.com>
2012-05-10 11:29:38 -0400

    better copy on the emails

--- spec/mailer/notification_mailer_spec.rb
+++ spec/mailer/notification_mailer_spec.rb
@@ -8,13 +8,13 @@ describe NotificationMailer do
       @comment = "how much would a wood chuck chuck..."
       @message = Factory.create(:message, :text => @comment, :user => @user_to)
       @conversation = Factory.create(:conversation, :messages => [@message])
-      @video = Factory.create(:video)
+      @video = Factory.create(:video, :title => 'tit')
       @frame = Factory.create(:frame, :roll=> Factory.create(:roll, :creator => @user_to), :video => @video)
       @email = NotificationMailer.comment_notification(@user_to, @user_from, @frame, @message)
     end
  
     it 'renders the subject' do
-      @email.subject.should eq(Settings::Email.comment_notification['subject'])
+      @email.subject.should == Settings::Email.comment_notification['subject'] % {:commenters_name => @user_from.nickname, :video_title => "tit"}
     end
     
     it 'renders the receiver email' do
@@ -35,14 +35,14 @@ describe NotificationMailer do
     before(:all) do
       @user_to = Factory.create(:user, :primary_email => 'your@mom.com')
       @user_from = Factory.create(:user, :primary_email => 'my@mom.com')
-      @video = Factory.create(:video)
+      @video = Factory.create(:video, :title => 'ti')
       @roll = Factory.create(:roll, :creator => @user_to)
       @frame = Factory.create(:frame, :video => @video, :roll => @roll)
       @email = NotificationMailer.upvote_notification(@user_to, @user_from, @frame)
     end
  
     it 'renders the subject' do
-      @email.subject.should eq(Settings::Email.upvote_notification['subject'])
+      @email.subject.should eq(Settings::Email.upvote_notification['subject'] % {:upvoters_name => @user_from.nickname, :video_title => 'ti' } )
     end
     
     it 'renders the receiver email' do
@@ -65,14 +65,14 @@ describe NotificationMailer do
       @new_user = Factory.create(:user)
       @old_roll = Factory.create(:roll, :creator => @old_user)
       @new_roll = Factory.create(:roll, :creator => @new_user)
-      @video = Factory.create(:video)
+      @video = Factory.create(:video, :title => 'tit')
       @old_frame = Factory.create(:frame, :creator => @old_user, :video => @video, :roll => @old_roll)
       @new_frame = Factory.create(:frame, :creator => @new_user, :video => @video, :roll => @new_roll)
       @email = NotificationMailer.reroll_notification(@old_frame, @new_frame)
     end
  
     it 'renders the subject' do
-      @email.subject.should eq(Settings::Email.reroll_notification['subject'])
+      @email.subject.should eq(Settings::Email.reroll_notification['subject'] % {:re_rollers_name => @new_user.nickname, :video_title => 'tit'})
     end
     
     it 'renders the receiver email' do
