9c074f7385623476688ae681ce6656726eb341f4
Mark Johnson <markjohnson@gmail.com>
2012-06-22 17:39:27 -0700

    Add code to fix conversations w/o frames.

--- lib/dev/conversation_video_fixer.rb
+++ lib/dev/conversation_video_fixer.rb
@@ -5,12 +5,13 @@
 # *Should only be used for DB repair/fixing* 
 #
 # Usage:
-#   Dev::ConversationVideoFixer.fix!
+#   Dev::ConversationVideoFixer.fixFramesAndVideos!
+#   Dev::ConversationVideoFixer.fixVideos!
 #
 module Dev
   class ConversationVideoFixer
     
-    def self.fix!
+    def self.fixVideos!
       conversationsToFix = Conversation.where(:video_id => nil)
       fixed = 1
       total = 1
@@ -43,6 +44,53 @@ module Dev
         end
         total += 1
       end if conversationsToFix
+
+      puts "-----------------------------"
+      puts " Total visited: ~#{total}"
+      puts " Total fixed: ~#{fixed}"
+      puts "-----------------------------"
+
     end
+
+    def self.fixFramesAndVideos!
+      conversationsToFix = Conversation.where(:video_id => nil, :frame_id => nil)
+      fixed = 1
+      total = 1
+      conversationsToFix.each do |c|
+        begin
+          if f = Frame.find(:conversation_id => c._id)
+            c.frame_id = f._id
+            c.video_id = f.video_id
+            begin
+              c.save
+              fixed += 1
+            rescue
+            end
+          else
+            puts "Could not find Frame..."
+          end
+        rescue
+          puts "Exception while looking up Frame..."
+        end
+        if total % 1000 == 0
+          puts "Iterated over ~#{total} conversations"
+        end
+        # throttle a little bit to give the DB time to catch its breath...
+        if fixed % 10000 == 0
+          puts "Fixed ~#{fixed} conversations... sleep(1)"
+          # hack so that we never accidentally sleep twice in a row
+          fixed += 1
+          sleep(1)
+        end
+        total += 1
+      end if conversationsToFix
+
+      puts "-----------------------------"
+      puts " Total visited: ~#{total}"
+      puts " Total fixed: ~#{fixed}"
+      puts "-----------------------------"
+
+    end
+
   end
 end
