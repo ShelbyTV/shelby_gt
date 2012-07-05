6c6d1b822b8d883abd14cb6899db501c491361d8
Michael Matyus <michael@maty.us>
2012-07-05 15:35:17 -0400

    templatization of revised emails

--- app/helpers/mail_helper.rb
+++ app/helpers/mail_helper.rb
@@ -2,16 +2,17 @@ module MailHelper
   
   def standard_html_email_wrapper
     <<-END_HTML
-      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-      <html xmlns="http://www.w3.org/1999/xhtml">
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html>
       <head>
-      	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       </head>
-      <body class="shelby">
-        #{yield}
+      <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="-webkit-text-size-adjust: none;margin: 0;padding: 0;background-color: #FAFAFA;width: 100%;">
+        <center>
+          #{yield}
+        </center>
       </body>
-      </html>
+    </html>
     END_HTML
   end
   
