620fa62cfd966d895ff41527c1584c5cc1b600aa
Michael Matyus <michael@maty.us>
2012-07-05 14:54:38 -0400

    normalized variables in notifiation_mailer, normalized html in emails, updated email yml

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -9,14 +9,18 @@ class NotificationMailer < ActionMailer::Base
 
     @user_to = user_to
     @user_from = user_from
-    @message = message
-    @frame = frame
-    
-    @frame_permalink = @frame.permalink
+    @user_from_name = (@user_from.name || @user_from.nickname)
     @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_from.id}/personal_roll"
+    
+    @frame = frame
+    @frame_title = @frame.video.title
+    @frame_permalink = @frame.permalink
+
+    @message = message
+
     mail :from => "Shelby.tv <#{Settings::Email.notification_sender}>", 
       :to => user_to.primary_email, 
-      :subject => Settings::Email.comment_notification['subject'] % { :commenters_name => (@user_from.name || @user_from.nickname), :video_title => @frame.video.title }
+      :subject => Settings::Email.comment_notification['subject'] % { :commenters_name => @user_from_name, :video_title => @frame_title }
   end
 
   def upvote_notification(user_to, user_from, frame)
@@ -24,13 +28,16 @@ class NotificationMailer < ActionMailer::Base
     
     @user_to = user_to
     @user_from = user_from
-    @frame = frame
-    @permalink = @frame.permalink
+    @user_from_name = (@user_from.name || @user_from.nickname)
     @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_from.id}/personal_roll"
+
+    @frame = frame
+    @frame_title = @frame.video.title
+    @frame_permalink = @frame.permalink
     
     mail :from => "Shelby.tv <#{Settings::Email.notification_sender}>", 
       :to => user_to.primary_email, 
-      :subject => Settings::Email.upvote_notification['subject'] % { :upvoters_name => (@user_from.name || @user_from.nickname), :video_title => @frame.video.title }
+      :subject => Settings::Email.upvote_notification['subject'] % { :upvoters_name => @user_from_name, :video_title => @frame_title }
   end
 
   def reroll_notification(old_frame, new_frame)
@@ -38,29 +45,34 @@ class NotificationMailer < ActionMailer::Base
 
     @user_to = old_frame.creator
     @user_from = new_frame.creator
+    @user_from_name = (@user_from.name || @user_from.nickname)
+    @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_from.id}/personal_roll"
 
     @old_frame = old_frame
     @new_frame = new_frame
+    @new_frame_title = @new_frame.video.title
     @new_frame_permalink = @new_frame.permalink
-    @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_from.id}/personal_roll"
     
     mail :from => "Shelby.tv <#{Settings::Email.notification_sender}>", 
       :to => @user_to.primary_email, 
-      :subject => Settings::Email.reroll_notification['subject'] % { :re_rollers_name => (@user_from.name || @user_from.nickname), :video_title => @new_frame.video.title }
+      :subject => Settings::Email.reroll_notification['subject'] % { :re_rollers_name => @user_from_name, :video_title => @new_frame_title }
   end
 
   def join_roll_notification(user_to, user_from, roll)
     sendgrid_category Settings::Email.join_roll_notification["category"]
 
     @user_to = user_to
-    @user_joined = user_from
+    @user_from = user_from
+    @user_from_name = (@user_from.name || @user_from.nickname)
+    @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_from.id}/personal_roll"
 
     @roll = roll
+    @roll_title = @roll.title
     @roll_permalink = @roll.permalink
-    @user_permalink = "#{Settings::Email.web_url_base}/user/#{@user_joined.id}/personal_roll"
+
     mail :from => "Shelby.tv <#{Settings::Email.notification_sender}>", 
       :to => @user_to.primary_email, 
-      :subject => (Settings::Email.join_roll_notification['subject'] % { :users_name => (@user_joined.name || @user_joined.nickname), :roll_title => @roll.title })
+      :subject => (Settings::Email.join_roll_notification['subject'] % { :users_name => @user_from_name, :roll_title => @roll_title })
   end
 
 end
