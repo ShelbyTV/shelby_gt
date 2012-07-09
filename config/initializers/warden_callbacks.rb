07d32e0e063b6a08689c98c72925e8ae5e4afc66
Henry Sztul <henry@sztul.com>
2012-07-09 10:40:25 -0400

    started work in the place where sessions and cookies need to be synced

--- config/initializers/warden_callbacks.rb
+++ config/initializers/warden_callbacks.rb
@@ -4,7 +4,30 @@
 Warden::Manager.before_logout do |user, auth, opts|
   auth.cookies[:_shelby_gt_common] = {
     :value => "authenticated_user_id=nil,csrf_token=nil",
-    :expires => 1.week.from_now,
+    :expires => 20.years.from_now,
     :domain => '.shelby.tv'
   }
 end
+
+####
+# Q: WHY ISNT THIS COOKIE BEING CLEARED WHEN A SESSION IS FUCKED (for whatever reason)??
+###
+
+=begin
+module ActionDispatch
+  class Request
+        
+    def self.reset_session
+
+      cookies[:_shelby_gt_common] = {
+        :value => "authenticated_user_id=nil,csrf_token=nil",
+        :expires => 20.years.from_now,
+        :domain => '.shelby.tv'
+      }
+
+      Rails.logger.info "====== RESET SESSION: #{cookies[:_shelby_gt_common]}"
+    end
+    
+  end
+end
+=end
