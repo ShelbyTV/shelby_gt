1f61efba52bebf68f825ea264f47a1ac2ec2092c
Dan Spinosa <spinosa@gmail.com>
2012-07-13 13:59:41 -0400

    detect nil roll and fix that when running ensure_roll_followings_mirrored

--- lib/utils/user_doctor.rb
+++ lib/utils/user_doctor.rb
@@ -25,13 +25,19 @@ module GT
     
     # For each of the user's rolls pointed to by a roll_following: make sure there's a matching following_user on the roll pointing to the user
     def self.ensure_roll_followings_mirrored(u)
+      should_clean_roll_followings = false
       u.roll_followings.each do |rf|
-        unless rf.roll.following_users.any? { |fu| fu.user_id == u.id }
-          #User had a roll following, but the roll it pointed to DID NOT have a matching following user
-          puts "User #{u.nickname} followed #{rf.roll.id}, needed mirroring following_user"
-          rf.roll.push :following_users => FollowingUser.new(:user => u).to_mongo
+        if rf.roll
+          unless rf.roll.following_users.any? { |fu| fu.user_id == u.id }
+            #User had a roll following, but the roll it pointed to DID NOT have a matching following user
+            puts "User #{u.nickname} followed #{rf.roll.id}, needed mirroring following_user"
+            rf.roll.push :following_users => FollowingUser.new(:user => u).to_mongo
+          end
+        else
+          should_clean_roll_followings = true
         end
       end
+      clean_roll_followings(u) if should_clean_roll_followings
     end
     
     def self.examine(u)
