ee0f45e4a966f7e7e7fd31af721749d2e8509621
Joshua Samberg <josh@shelby.tv>
2012-07-20 12:23:41 -0400

    Fixed facebook posting

--- lib/social_postings/facebook_posting.rb
+++ lib/social_postings/facebook_posting.rb
@@ -13,26 +13,20 @@ module SocialPosting
       end
       
       def post_comment(message, fb_post_id=nil, entity=nil)
-        if Settings::Facebook.should_send_post
-          # once our share action and roll object are approved by facebook, this will simplify to:
-          # return !!GT::OpenGraph.send_action('share', @user, entity, message)
 
-          # send OG action to FB
-          #ShelbyGT_EM.next_tick { GT::OpenGraph.send_action('share', @user, entity, message) }
-          
-          if entity.is_a? Roll            
-            return !!facebook_client.put_object("me","feed",{
+        if entity.is_a? Roll
+          post_object = {
               :message => message, 
               :link => entity.short_links[:facebook],
-              :picture => entity.thumbnail_url,
+              :picture => entity.first_frame_thumbnail_url,
               :name => entity.title,
               :caption => "via Shelby.TV",
               :description => entity.title,
               :application => Settings::Facebook.app_name,
               :icon => Settings::Facebook.fb_application_icon
-              })
-          elsif entity.is_a? Frame
-            return !!facebook_client.put_object("me","feed",{
+              }
+        elsif entity.is_a? Frame
+          post_object = {
               :message => message, 
               :link => entity.short_links[:facebook],
               :picture => entity.video.thumbnail_url,
@@ -41,13 +35,21 @@ module SocialPosting
               :description => entity.video.description,
               :application => Settings::Facebook.app_name,
               :icon => Settings::Facebook.fb_application_icon
-              })
-          else
-            return !!facebook_client.put_object("me", "feed", {
+              }
+        else
+          post_object = {
               :message => message,
               :attribution => Settings::Facebook.app_name
-              })
-          end
+              }
+        end
+
+        if Settings::Facebook.should_send_post
+          # once our share action and roll object are approved by facebook, this will simplify to:
+          # return !!GT::OpenGraph.send_action('share', @user, entity, message)
+
+          # send OG action to FB
+          #ShelbyGT_EM.next_tick { GT::OpenGraph.send_action('share', @user, entity, message) }
+          return !!facebook_client.put_object("me","feed", post_object)
         else
           Rails.logger.info "In production, would have facebooked: #{message} w/ post_id #{fb_post_id}"
           return true
