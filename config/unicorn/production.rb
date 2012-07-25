09050d5e632d9cd9c78c00bdffc7a8dca4edd27d
Henry Sztul <henry@sztul.com>
2012-07-25 13:54:55 -0400

    udpate unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -21,8 +21,8 @@ working_directory app_path
 rails_env = ENV['RAILS_ENV'] || 'production'
 
 # Log everything to one file
-stderr_path "log/unicorn.log"
-stdout_path "log/unicorn.log"
+stderr_path "#{app_path}/log/unicorn.log"
+stdout_path "#{app_path}/log/unicorn.log"
 
 # Set master PID location
 pid "#{app_path}/../shared/pids/unicorn.pid"
