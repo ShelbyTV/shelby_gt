c2deb4ce4b22440a1a3c610c2f1ae439ee329a9f
Dan Spinosa <spinosa@gmail.com>
2012-08-28 16:07:46 -0400

    use deterministic avatar filename

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -9,23 +9,20 @@ Paperclip::Attachment.default_options[:s3_credentials] = {
     :secret_access_key => Settings::Paperclip.secret_access_key
     }
 Paperclip::Attachment.default_options[:bucket] = Settings::Paperclip.bucket
-Paperclip::Attachment.default_options[:path] = "/:style/:id/:filename"
+Paperclip::Attachment.default_options[:path] = "/:style/:id"
 
-# The paperclip plugin provides lots of view helpers to construct the avatar URL
-# We will not be passing all of these over the API
+# To simplify things, a user's Shelby avatar S3 file name is deterministic based on the user's id.
 #
 # Using the format of :path above, here's how you contruct an avatar URL:
-#   http://s3.amazonaws.com/shelby-gt-user-avatars/<style>/<user.id>/<user.avatar_file_name>
+#   http://s3.amazonaws.com/shelby-gt-user-avatars/<style>/<user.id>
 # Where <style> is one of the pre-defined sizes is user.rb, currently:
 # :styles => { :sq192x192 => "192x192#", :sq48x48 => "48x48#" }
 #
 # Example of a large thumbnail URL:
-#   http://s3.amazonaws.com/dev-shelby-gt-user-avatars/sq192x192/4fa141009fb5ba2b2b000002/dan_ani_with_cow.gif?2012
+#   http://s3.amazonaws.com/dev-shelby-gt-user-avatars/sq192x192/4fa141009fb5ba2b2b000002
 #
 # You can also get at the originally uploaded image (with original aspect ratio) using the "original" style:
-#   http://s3.amazonaws.com/dev-shelby-gt-user-avatars/original/4fa141009fb5ba2b2b000002/dan_ani_with_cow.gif?2012
-#
-# (FYI: Not sure why paperclip appends the year of the upload to the URL, assuming some sort of cache busting, but it's not necessary)
+#   http://s3.amazonaws.com/dev-shelby-gt-user-avatars/original/4fa141009fb5ba2b2b000002
 
 
 
