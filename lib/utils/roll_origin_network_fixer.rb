e52f010b000f4e2cdc33830fb374e7281ea23791
Mark Johnson <markjohnson@gmail.com>
2012-09-10 22:54:34 -0700

    Keep iterating even when there are exceptions.

--- lib/utils/roll_origin_network_fixer.rb
+++ lib/utils/roll_origin_network_fixer.rb
@@ -13,20 +13,28 @@ module Dev
     def self.fix!
       total = 0
       fixed = 0
-      rollsToFix = Roll.where(:roll_type => Roll::TYPES[:special_public], :origin_network => nil)
-      rollsToFix.each do |r|
-        total += 1
-        if u = User.find(r.creator_id)
-          if u.authentications.count == 1
-            r.origin_network = u.authentications.first.provider
-            r.save
-            fixed += 1
+      while (true) {
+        begin
+          rollsToFix = Roll.where(:roll_type => Roll::TYPES[:special_public], :origin_network => nil).skip(total - fixed)
+          break if !rollsToFix
+
+          rollsToFix.each do |r|
+            total += 1
+            if u = User.find(r.creator_id)
+              if u.authentications.count == 1
+                r.origin_network = u.authentications.first.provider
+                r.save
+                fixed += 1
+              end
+            end
+            if total % 10000 == 0
+              puts "Total: #{total}, Fixed: #{fixed}"
+            end
           end
+        rescue
+          puts "Exception! Looping again..."
         end
-        if total % 10000 == 0
-          puts "Total: #{total}, Fixed: #{fixed}"
-        end
-      end if rollsToFix
+      end
     end
 
   end
