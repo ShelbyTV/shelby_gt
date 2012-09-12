5211d56acb9000d2b6fd9094255c0fcae5f6168a
Mark Johnson <markjohnson@gmail.com>
2012-09-12 13:56:52 -0700

    Add script for fixing NOS user public roll types.

---
+++ lib/utils/nos_user_roll_type_fixer.rb
@@ -0,0 +1,38 @@
+# This was written to fix up all the old NOS users's roll properties so they are treated as user rolls in GT.
+# Left checked in as demo code / in case it's useful in the near future.
+#
+# *Should only be used for DB repair/fixing* 
+#
+# Usage:
+#   Dev::NosUserRollTypeFixer.fix!
+#
+module Dev
+  class NosUserRollTypeFixer
+    
+    def self.fix!
+      total = 0
+      fixed = 0
+      while (true)
+        begin
+          usersToFix = User.where(:faux => User::FAUX_STATUS[:false]).skip(total - fixed)
+          break if !usersToFix
+
+          usersToFix.each do |u|
+            total += 1
+            if r = Roll.find(u.public_roll_id)
+              r.roll_type = Roll::TYPES[:special_public_real_user]
+              r.save
+              fixed += 1
+            end
+            if total % 1000 == 0
+              puts "Total: #{total}, Fixed: #{fixed}"
+            end
+          end
+        rescue
+          puts "Exception! Looping again..."
+        end
+      end
+    end
+
+  end
+end
