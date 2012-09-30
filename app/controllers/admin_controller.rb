77f4565e43b32265f7ce87b96e730c36cb38d9ad
Henry Sztul <henry@sztul.com>
2012-09-30 11:44:08 -0400

    new web version of daily email

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,39 @@
+require 'rhombus'
+
 class AdminController < ApplicationController    
   before_filter :is_admin?
   
   def index
     
+  end
+  
+  def new_users 
+    rhombus = Rhombus.new('shelby', '_rhombus_gt')
+    
+    # create a bson id object that represents the beginning of the day
+    start_at = Time.zone.now.beginning_of_day
+    time_as_id = BSON::ObjectId.from_time(start_at)
+    
+    # find all new users as of today that are real users
+    @new_new_users = User.where('id' => {'$gte' => time_as_id}, :faux => 0 ).all
+    @converted_new_users = User.where('id' => {'$gte' => time_as_id}, :faux => 2 ).all
+
+    # get recent gt enabled users from rhombus
+    rhombus_resp = JSON.parse(rhombus.get('/smembers', {:args => ['new_gt_enabled_users'], :limit=>24}))
+    gt_enabled_ids = rhombus_resp["error"] ? [] : rhombus_resp["data"].values.flatten 
+    @new_gt_enabled_users = User.find(gt_enabled_ids)
+    # so we can distinguish these in the html.erb
+    @new_gt_enabled_users.map! {|u| u.faux = 9; u }
+    
+    @new_gt_enabled_users.delete_if { |u| @new_new_users.include? u }
+      
+    # send email summary if there are new users
+    if !@new_new_users.concat(@new_gt_enabled_users).concat(@converted_new_users).empty?
+      @all_new_users = @new_new_users.concat(@converted_new_users).concat(@new_gt_enabled_users)
+      #temp
+    end
+    
+    @temp = User.all[0..5]
   end
   
   # shows facts and tidbits about a user
