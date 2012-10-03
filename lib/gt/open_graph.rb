1aa2d561ca17cf1116b66404feaf40592d1637f8
Henry Sztul <henry@sztul.com>
2012-10-03 12:26:21 -0400

    allowing anyone that has opted in to post fb open graph actions

--- lib/gt/open_graph.rb
+++ lib/gt/open_graph.rb
@@ -9,7 +9,7 @@ module GT
       Rails.logger.info("[GT::OpenGraph] Would have sent OG action: #{action}") unless "production" == Rails.env
       
       # make sure the user wants us to send actions to facebook open graph
-      return unless (["henry", "spinosa", "reece", "chris", "lauren"].include?(user.nickname)) and user.has_provider("facebook") and user.preferences.can_post_to_open_graph?
+      return unless user.has_provider("facebook") and user.preferences.can_post_to_open_graph?
       
       og_object = {}
       
