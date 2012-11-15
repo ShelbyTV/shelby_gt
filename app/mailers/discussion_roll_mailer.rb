9ff90d572bf42ae9c8bd1c2e31e3976a97e61442
Dan Spinosa <spinosa@gmail.com>
2012-11-15 14:37:41 -0500

    email tweaks, fixes

--- app/mailers/discussion_roll_mailer.rb
+++ app/mailers/discussion_roll_mailer.rb
@@ -13,20 +13,29 @@ class DiscussionRollMailer < ActionMailer::Base
     sendgrid_ganalytics_options(:utm_source => 'discussion_roll', :utm_medium => 'notification', :utm_campaign => "roll_#{roll.id}")
     
     @roll = roll
+    @from_name = from_name_for(@roll)
     @recipient = recipient
     @token = token
     
-    mail :from => Settings::Email.discussion_roll['from'], 
+    mail :from => "#{@from_name} <#{Settings::Email.discussion_roll['from_email']}>", 
       :to => email_to, 
       :subject => subject_for(conversation_with)
   end
   
   private
   
+    # Subject must be unique and identical every time an email is sent for a given discussion roll
+    # This way email clients can nicely group the emails into a conversation
     def subject_for(conversation_with)
-      # Subject must be unique and identical every time an email is sent for a given discussion roll
-      # This way email clients can nicely group the emails into a conversation
-      conversation_with.map { |p| p.is_a?(User) ? p.nickname : p } .join(", ") + " -- Video Conversation"
+      "Shelby Chat: " + (conversation_with.map { |p| p.is_a?(User) ? p.nickname : p } .join(", "))
+    end
+    
+    # The displayed name should be whomever left the last comment
+    # (while the emails address will be contact@shelby.tv)
+    def from_name_for(roll)
+      f = Frame.where(:roll_id => roll.id).order(:score.desc).first
+      return nil unless f and f.conversation and !f.conversation.messages.empty?
+      return f.conversation.messages[-1].nickname
     end
   
 end
