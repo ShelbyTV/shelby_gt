f40904dc59efefed033a0608640b359a64846a6b
Dan Spinosa <spinosa@gmail.com>
2012-11-19 19:42:06 -0500

    basic shelby chat email

--- app/controllers/v1/discussion_roll_controller.rb
+++ app/controllers/v1/discussion_roll_controller.rb
@@ -54,7 +54,7 @@ class V1::DiscussionRollController < ApplicationController
     frame.conversation.save
 
   	#sends emails to all but the poster
-  	ShelbyGT_EM.next_tick { GT::NotificationManager.check_and_send_discussion_roll_notification(@roll, current_user) }
+  	ShelbyGT_EM.next_tick { GT::NotificationManager.send_discussion_roll_notifications(@roll, current_user) }
     
     @status =  200
     render "/v1/roll/show"
@@ -153,7 +153,7 @@ class V1::DiscussionRollController < ApplicationController
 
     if @conversation.save
       #sends emails to all but the poster
-    	ShelbyGT_EM.next_tick { GT::NotificationManager.check_and_send_discussion_roll_notification(roll, poster) }
+    	ShelbyGT_EM.next_tick { GT::NotificationManager.send_discussion_roll_notifications(roll, poster) }
     	
       @status =  200
       # Render an array of Frames if new ones were created, or the single updated Conversation
--- app/mailers/discussion_roll_mailer.rb
+++ app/mailers/discussion_roll_mailer.rb
@@ -8,14 +8,20 @@ class DiscussionRollMailer < ActionMailer::Base
   #   ie. [User1, "email1@gmail.com", User2, User3, "email2@gmail.com", ...]
   # token is used to authenticate and authorize users when viewing and posting messages to this discussion roll
   # recipient is the user id (as string) or email address of the recipient, as stored in Roll.discussion_roll_participants
-  def state_of_discussion_roll(roll, email_to, recipient, conversation_with, token)
+  #
+  # poster may be a User or an email address
+  def state_of_discussion_roll(roll, email_to, recipient, poster, conversation_with, token)
     sendgrid_category Settings::Email.discussion_roll["category"]
     sendgrid_ganalytics_options(:utm_source => 'discussion_roll', :utm_medium => 'notification', :utm_campaign => "roll_#{roll.id}")
     
-    @roll = roll
-    @from_name = from_name_for(@roll)
     @recipient = recipient
     @token = token
+    @poster = poster
+    @from_name = poster.is_a?(User) ? poster.nickname : poster
+    @roll = roll
+    @most_recent_frame = Frame.where(:roll_id => roll.id).order(:score.desc).first
+    @message = @most_recent_frame.conversation.messages[-1]
+    @permalink = "http://shelby.tv/chat/#{@roll.id}?u=#{CGI.escape(@recipient)}&t=#{CGI.escape(CGI.escape(@token))}"
     
     mail :from => "#{@from_name} <#{Settings::Email.discussion_roll['from_email']}>", 
       :to => email_to, 
@@ -28,14 +34,6 @@ class DiscussionRollMailer < ActionMailer::Base
     # This way email clients can nicely group the emails into a conversation
     def subject_for(conversation_with)
       "Shelby Chat: " + (conversation_with.map { |p| p.is_a?(User) ? p.nickname : p } .join(", "))
-    end
-    
-    # The displayed name should be whomever left the last comment
-    # (while the emails address will be contact@shelby.tv)
-    def from_name_for(roll)
-      f = Frame.where(:roll_id => roll.id).order(:score.desc).first
-      return nil unless f and f.conversation and !f.conversation.messages.empty?
-      return f.conversation.messages[-1].nickname
     end
   
 end
