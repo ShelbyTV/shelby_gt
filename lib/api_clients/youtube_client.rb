7a11782955ebd13b8558892513f07b7c7d5c631e
Henry Sztul <henry@sztul.com>
2012-11-21 10:45:23 -0500

    adding youtube

---
+++ lib/api_clients/youtube_client.rb
@@ -0,0 +1,34 @@
+module APIClients
+  class Youtube
+    
+    def self.search(query, limit=10, page=1)
+      raise ArgumentError, "must supply valid query" unless query.is_a?(String)
+      
+      return {:status => "ok", :limit => limit, :page => page, :videos => [] } if Rails.env == "test"
+      
+      response = client.videos_by(:query => query, :page => page, :per_page => limit)
+      
+      response = client.search("query", { :query => "timelapse", :page => page, :per_page => limit.to_s, :full_response => "1", :sort => "relevant" })
+      if response["stat"] == "ok"
+        return {  :status => response["stat"],
+                  :limit => limit,
+                  :page => page,
+                  :videos => response["videos"]["video"]
+                }
+      else
+        return { :status => response["stat"] }
+      end
+    end
+    
+    private
+    
+      def self.client
+        unless @client
+          @client = YouTubeIt::Client.new(:dev_key => Settings::Youtube.developer_key)
+        end
+        @client
+      end
+    
+    
+  end
+end
