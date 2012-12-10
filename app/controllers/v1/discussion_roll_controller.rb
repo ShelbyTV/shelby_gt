86528d55dc0ebff493c7e6052552713c709c89b3
Dan Spinosa <spinosa@gmail.com>
2012-12-10 16:15:11 -0500

    user identifier for token necessary

--- app/controllers/v1/discussion_roll_controller.rb
+++ app/controllers/v1/discussion_roll_controller.rb
@@ -87,6 +87,7 @@ class V1::DiscussionRollController < ApplicationController
   	#sends emails to all (including the poster)
   	ShelbyGT_EM.next_tick { GT::NotificationManager.send_discussion_roll_notifications(@roll, current_user, true) }
     
+    @user_identifier = current_user.id.to_s
     @insert_discussion_roll_access_token = true
     @status =  200
     render "/v1/roll/show"
