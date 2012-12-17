3903f4e0e04b0360834e4a23b414911853b38c05
Dan Spinosa <spinosa@gmail.com>
2012-12-17 16:14:05 -0500

    route for posting js errors to new relic

---
+++ app/controllers/v1/javascript_errors_controller.rb
@@ -0,0 +1,28 @@
+class V1::JavascriptErrorsController < ApplicationController
+  layout false
+  
+  ##
+  # Sends an error to New Relic about a javascript error that occurred in a front end.
+  #   AUTHENTICATION IGNORED
+  #
+  # [POST] /v1/js_err
+  #
+  # @param [Optional, String] error_message The JS error message from the browser
+  # @param [Optional, String] uri The page URI as reported by the browser (window.location.href)
+  # @param [Optional, String] client The client generating this error (default: WebFrontEnd) -- Use this to specify TV, staging, etc.
+  # @param [Optional, String] * Everything else sent with params will be sent to New Relic as custom params
+  def create
+    error_msg = params.delete(:error_message) || "(client needs to send 'error_message' parameter)"
+    uri = params.delete(:uri) || "(client needs to send 'uri' parameter)"
+    client = params.delete(:client) || "WebFrontEnd"
+    
+    NewRelic::Agent.notice_error("Javascript error: #{error_msg}", {
+      :metric => "Custom/Javascript/#{client}",
+      :uri => uri, 
+      :custom_params => params})
+      
+    @status = 200
+    render 'v1/blank'
+  end
+  
+end
