aef0e4eff286393a0321e1b493ea84e32783546a
Henry <henry@sztul.com>
2012-12-19 15:41:25 -0500

    making sure call to Pusher class is always global

--- app/controllers/v1/remote_control_controller.rb
+++ app/controllers/v1/remote_control_controller.rb
@@ -1,14 +1,14 @@
-class V1::RemoteControlController < ApplicationController  
-  
+class V1::RemoteControlController < ApplicationController
+
   def show
-    
+
   end
 
   ################
   # The :id is the code gotten from calling remote_control#create
   #  REQUIRED :
   #    params[:command] is the command to send from the remote to the screen
-  #  OPTIONAL : 
+  #  OPTIONAL :
   #    params[:data] can be data passed to the screen from the remote (for searching?)
   def update
     if rc = RemoteControl.where(:code => params[:id]).all
@@ -16,7 +16,7 @@ class V1::RemoteControlController < ApplicationController
       @remote_control = rc.first
       @command = params[:command]
       @data = params[:data]
-      ShelbyGT_EM.next_tick do 
+      ShelbyGT_EM.next_tick do
         pusher_client.trigger('remote-'+@remote_control.code, @command, @data)
       end
       @status = 200
@@ -24,7 +24,7 @@ class V1::RemoteControlController < ApplicationController
       render_error(404, "could not find a remote control with that id. :<>")
     end
   end
-  
+
   def create
     if @remote_control = RemoteControl.create()
       @status = 200
@@ -32,11 +32,11 @@ class V1::RemoteControlController < ApplicationController
       render_error(404, "could not create the remote control code :/")
     end
   end
-  
+
   private
     def pusher_client
       unless @pusher_client
-        @pusher_client = Pusher::Client.new({
+        @pusher_client = ::Pusher::Client.new({
           app_id: Settings::Pusher.id,
           key: Settings::Pusher.key,
           secret: Settings::Pusher.secret
@@ -44,5 +44,5 @@ class V1::RemoteControlController < ApplicationController
       end
       @pusher_client
     end
-  
+
 end
