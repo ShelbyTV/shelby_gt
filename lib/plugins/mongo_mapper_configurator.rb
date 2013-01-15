627ec4cf22152b60050f819954639913e9c423d8
Dan Spinosa <spinosa@gmail.com>
2013-01-15 13:46:21 -0500

    upgrade mongo driver class, set write concern to 0 to mimic old behavior

--- lib/plugins/mongo_mapper_configurator.rb
+++ lib/plugins/mongo_mapper_configurator.rb
@@ -6,9 +6,9 @@ module Plugins
       def configure_mongomapper(settings)
         if settings['db_hosts']
           # Starting with a proper Hash and merging into it b/c Mongo::Connection checks if the class is Hash, which it isn't when using Settings
-          connection Mongo::ReplSetConnection.new( settings.db_hosts, {}.merge(settings.db_options.merge(Settings::Mongo.db_options)) )
+          connection Mongo::MongoReplicaSetClient.new( settings.db_hosts, {}.merge(settings.db_options.merge(Settings::Mongo.db_options)) )
         else
-          connection Mongo::Connection.new(settings.db_host, settings.db_port, {}.merge(settings.db_options.merge(Settings::Mongo.db_options)) )
+          connection Mongo::MongoClient.new(settings.db_host, settings.db_port, {}.merge(settings.db_options.merge(Settings::Mongo.db_options)) )
         end
         set_database_name settings.db_name
         database.authenticate(settings.db_username, settings.db_password) if settings['db_username'] && settings['db_password']
