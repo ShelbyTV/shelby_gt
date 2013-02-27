d9a7fca2ce28851a0fc3e1852886ec06bdb884c3
Dan Spinosa <spinosa@gmail.com>
2013-02-27 10:58:40 -0500

    properly truncate tweets, accounting for t.co shortlinking.  Fixes #186

--- lib/gt/social_post_formatter.rb
+++ lib/gt/social_post_formatter.rb
@@ -1,10 +1,22 @@
 # encoding: UTF-8
 module GT
   class SocialPostFormatter
+    MAX_TWEET_LENGTH = 140
+    TWITTER_SHORTLINK_LENGTH = 22
     
+    # Twitter's link shortener effectively turns all links into 22 characters
+    # Need to account for each link in text, plus our own link, plus our own added characters
     def self.format_for_twitter(text, links)
-      # truncate text so that our link can fit fo sure and be < 140
-      t = text.length > 117 ? "#{text[0..116]}…" : text
+      shelby_added_length = 2
+      max_length = MAX_TWEET_LENGTH - TWITTER_SHORTLINK_LENGTH - shelby_added_length
+      
+      URI.extract(text, ["http", "https"]).each do |link|
+        # we get space back if our link is shorter than twitters
+        # otherwise we have to give up more space
+        max_length -= (TWITTER_SHORTLINK_LENGTH - link.length)
+      end
+      
+      t = text.length > max_length ? "#{text[0...max_length]}…" : text
       t += " #{links["twitter"]}" if links["twitter"]
       return t
     end
--- spec/lib/gt/unit/social_post_formatter_spec.rb
+++ spec/lib/gt/unit/social_post_formatter_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 require 'social_post_formatter'
 
@@ -8,10 +9,30 @@ describe GT::SocialPostFormatter do
       t = ""
       150.times.each {|d| t+="t"}
       
-      short_links = {"twitter" => "http://shl.by/12345"}
+      # twitter turns all links into 22 characters, so use 22 characters below
+      short_links = {"twitter" => "http://shl.by/12345678"}
       text = GT::SocialPostFormatter.format_for_twitter(t, short_links)
       (text.length <= 140).should eq(true)
     end
+    
+    it "should account for inline links under 22 characters" do
+      t = "well... don't build your house on the other side of http://aol.com ! ➔@superdupersecret: Flying car crash-landed in roof of house"
+      
+      # twitter turns all links into 22 characters, so use 22 characters below
+      short_links = {"twitter"=>"http://shl.by/12345678"}
+      text = GT::SocialPostFormatter.format_for_twitter(t, short_links)
+      (text.length <= 140).should eq(true)
+    end
+    
+    it "should account for inline links over 22 characters" do
+      t = "well... don't build your house on the other side of http://aol.com/and-this-link-is-super-long-which-is-okay-bc-it-will-be-shortened ! ➔@superdupersecret: Flying car crash-landed in roof of house"
+      
+      # twitter turns all links into 22 characters, so use 22 characters below
+      short_links = {"twitter"=>"http://shl.by/12345678"}
+      text = GT::SocialPostFormatter.format_for_twitter(t, short_links)
+      (text.length == 198).should eq(true)
+    end
+    
   end
 
 end
