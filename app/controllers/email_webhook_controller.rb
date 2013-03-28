4df50a6c37b3a89d8cbbc0db0d3b52b72ffd0671
Joshua Samberg <josh@shelby.tv>
2013-03-28 16:52:49 -0400

    Logging to debug email processing

--- app/controllers/email_webhook_controller.rb
+++ app/controllers/email_webhook_controller.rb
@@ -8,14 +8,20 @@ class EmailWebhookController < ApplicationController
       mail = Mail.read_from_string(params[:headers])
       mail_to = mail.to && mail.to[0]
       # so far, we only support email addressed to roll@volumevolume.com
+      Rails.logger.info "mail_to #{mail_to}"
       if mail_to && mail_to.casecmp("#{Settings::EmailHook.email_user_keys['roll']}.#{Settings::EmailHook.email_hook_domain}") == 0
         if mail.from
+          Rails.logger.info "mail.from #{mail.from}"
           mail.from.each do |address|
+            Rails.logger.info "address #{address}"
             # figure out who the email is from and match it up with a shelby user
             if user = User.find_by_primary_email(address)
+              Rails.logger.info "user #{user.inspect}"
               # if we've got a user, then parse the email for links
               if (params[:text])
+                Rails.logger.info "text #{params[:text]}"
                 params[:text].scan(/\b(?:https?:\/\/|www\.)\S+\b/) do |link_match|
+                  Rails.logger.info "link #{link_match}"
                   # try to create a video for that link
                   if video = GT::VideoManager.get_or_create_videos_for_url(link_match)[:videos][0]
                     # if we get a video, make a frame out of it
