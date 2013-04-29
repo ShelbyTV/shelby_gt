9f138409ca4a5638d0ada6806768c13dbb92ae0b
Henry <henry@sztul.com>
2013-04-29 09:54:56 -0400

    handling case where user image is null

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   # future times => "just now"
   # < 1 minute => "just now"
   # 1m - 59m => "Xm"
@@ -13,9 +13,9 @@ module ApplicationHelper
       return ""
     end
     distance_in_minutes = (((to_time - from_time))/60).round
-    
+
     return "just now" if distance_in_minutes <= 1
-    
+
     case distance_in_minutes
     when 1..59 then "#{distance_in_minutes}m ago"
     when 60..720 then "#{distance_in_minutes/60}h ago"
@@ -23,11 +23,13 @@ module ApplicationHelper
       from_time.respond_to?(:strftime) ? from_time.strftime("%b %-d") : ""
     end
   end
-  
+
   #valid avatar_size options are "small", "large", "original"
   def avatar_url_for_user(user, avatar_size="small")
     if user.avatar?
       return user.shelby_avatar_url(avatar_size)
+    elsif user.avatar? and user.avatar == "null"
+        "#{Settings::ShelbyAPI.web_root}/images/assets/avatar.png"
     else
       return user.user_image_original || user.user_image || "#{Settings::ShelbyAPI.web_root}/images/assets/avatar.png"
     end
@@ -38,11 +40,11 @@ module ApplicationHelper
       size = "sq48x48" if avatar_size == "small"
       size = "sq192x192" if avatar_size == "large"
       size = "original" if avatar_size == "original"
-      
+
       return "http://s3.amazonaws.com/#{Settings::Paperclip.user_avatar_bucket}/#{size}/#{message.user_id}"
     else
       return message.user_image_url
     end
   end
-  
+
 end
