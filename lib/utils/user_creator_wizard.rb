e3d5bfccb65e22c92a7570b20a835a4987bef5e5
Joshua Samberg <josh@shelby.tv>
2013-05-16 15:09:55 -0400

    Add option to create youtube BotRoll user in UserCreatorWizard

--- lib/utils/user_creator_wizard.rb
+++ lib/utils/user_creator_wizard.rb
@@ -6,13 +6,25 @@ module Dev
 
     def self.create_user!
 
-      nickname = ask('Enter a username: ')
-      password = ask("Enter a password:  ") { |q| q.echo = "x" }
+      nickname = ask('Enter a username: ') do |q|
+        q.validate = lambda { |a| a.length > 0 }
+        q.responses[:not_valid] = "You didn't enter a username. Try again please."
+      end
+      password = ask("Enter a password: ") { |q| q.echo = "x" }
       email = ask("Enter an email address for the user: ")
       name = ask("Enter the name of the user: ")
-      has_avatar = ask("Do you have an avatar for this user? ") { |q| q.default = "yes" }
-      avatar = ask("enter the url of the avatar: ") if has_avatar == "yes"
-      service_user = ask("Is the user a 'service' user? ") { |q| q.default = "yes" }
+      has_avatar = agree("Do you have an avatar for this user? ") { |q| q.default = "n" }
+      avatar = ask("enter the url of the avatar: ") if has_avatar
+      youtube_user = agree('Will this be a youtube BotRoll user? ') { |q| q.default = "n"}
+      if youtube_user
+        service_user = true
+        youtube_username = ask('Enter a youtube username: ') do |q|
+          q.validate = lambda { |a| a.length > 0 }
+          q.responses[:not_valid] = "You didn't enter a username. Try again please."
+        end
+      else
+        service_user = agree("Is the user a 'service' user? ") { |q| q.default = "y" }
+      end
 
       user_params = {
         nickname: nickname,
@@ -25,10 +37,35 @@ module Dev
       if @user and @user.errors.empty? and @user.valid?
         @user.user_image = avatar if has_avatar
         @user.ensure_authentication_token!
-        @user.user_type = 3 if service_user.downcase == "yes"
-        puts "[SUCCESS] #{@user.name} created: \n #{@user.inspect}" if @user.save
+        @user.user_type = 3 if service_user
+        if @user.save
+          puts "[SUCCESS] #{@user.name} created: \n #{@user.inspect}"
+          if youtube_user
+            puts "Configuring youtube BotRoll via audrey2"
+            begin
+              response = HTTParty.post("#{Settings::Audrey2.api_url}/v1/feeds", {:body =>
+                {
+                  :type => 'youtube',
+                  :id => youtube_username,
+                  :auth_token => @user.authentication_token,
+                  :roll_id => @user.public_roll_id.to_s
+                }
+              })
+            rescue Exception => e
+              puts "[FAILURE] audrey2 request failed: \n #{e.inspect}"
+              return
+            end
+            if response.code == 200
+              puts "[SUCCESS] audrey2 API responded with: \n #{response.code} -- #{response.body}"
+            else
+              puts "[FAILURE] audrey2 API responded with: \n #{response.code} -- #{response.body}"
+            end
+          end
+        else
+          puts "[FAILURE] Something went horribly wrong: #{@user.errors.messages}"
+        end
       else
-        puts "[FAILURE] Something went horribly wrong: #{@user.errors}"
+        puts "[FAILURE] Something went horribly wrong: #{@user.errors.messages}"
       end
     end
 
