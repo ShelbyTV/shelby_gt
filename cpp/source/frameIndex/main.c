9119cf06bb386594f81b0574a0fa843cc4c45fe0
Joshua Samberg <josh@shelby.tv>
2013-06-05 11:24:04 -0400

    More logging for roll/:id/frames C code in video output section

--- cpp/source/frameIndex/main.c
+++ cpp/source/frameIndex/main.c
@@ -223,11 +223,26 @@ void printJsonVideo(sobContext sob, mrjsonContext context, bson *video)
       SOB_VIDEO_FIRST_UNPLAYABLE_AT,
       SOB_VIDEO_LAST_UNPLAYABLE_AT
    };
+   bson_oid_t videoOid;
+
+   sobBsonOidField(SOB_VIDEO,
+                   SOB_VIDEO_ID,
+                   video,
+                   &videoOid);
+
+   char videoIdString[25];
+
+   bson_oid_to_string(&videoOid, videoIdString);
+   sobLog("Printing JSON for video: %s", videoIdString);
+
+   sobLog("Printing video standard fields");
 
    sobPrintAttributes(context,
                       video,
                       videoAttributes,
                       sizeof(videoAttributes) / sizeof(sobField));
+
+   sobLog("Printing video recommendations array");
 
    sobPrintSubobjectArray(sob,
                           context,
