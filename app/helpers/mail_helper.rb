3830a91e495ec23340c3a4c771a9e9be30e7022b
Michael Matyus <michael@maty.us>
2013-06-17 17:45:54 -0400

    saving progress on email revision

--- app/helpers/mail_helper.rb
+++ app/helpers/mail_helper.rb
@@ -1,5 +1,5 @@
 module MailHelper
-  
+
   def standard_html_email_wrapper
     <<-END_HTML
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -15,5 +15,5 @@ module MailHelper
     </html>
     END_HTML
   end
-  
+
 end
--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -3,14 +3,14 @@ if Rails.env.development?
 
       def reroll_notification #old_roll, new_roll
 
-        old_user = User.all.first
+        old_user               = User.all.first
         old_user.primary_email = "test@test.com"
-        old_roll = old_user.public_roll
-        old_video = Video.all.first
-        old_frame = Factory.create(:frame, :roll => old_roll, :video => old_video, :creator => old_user)
+        old_roll               = old_user.public_roll
+        old_video              = Video.all.first
+        old_frame              = Factory.create(:frame, :roll => old_roll, :video => old_video, :creator => old_user)
 
-        new_user = User.all.last
-        new_roll = new_user.public_roll
+        new_user  = User.all.last
+        new_roll  = new_user.public_roll
         new_video = Video.all.last
         new_frame = Factory.create(:frame, :roll => new_roll, :video => new_video, :creator => new_user)
 
@@ -22,11 +22,11 @@ if Rails.env.development?
       end
 
       def like_notification #(user_to, frame, user_from=nil)
-        user_to = User.all.first
+        user_to   = User.all.first
         user_from = User.all.last
-        roll = user_to.public_roll
-        video = Video.all.first
-        frame = Factory.create(:frame, :roll => roll, :video => video, :creator => user_to)
+        roll      = user_to.public_roll
+        video     = Video.all.first
+        frame     = Factory.create(:frame, :roll => roll, :video => video, :creator => user_to)
 
         NotificationMailer.like_notification(user_to, frame, user_from)
 
@@ -34,9 +34,9 @@ if Rails.env.development?
       end
 
       def join_roll_notification # user_to, user_from, roll
-        user_to = User.all.first
-        user_from = User.all.last
-        roll = user_to.public_roll
+        user_to    = User.all.first
+        user_from  = User.all.last
+        roll       = user_to.public_roll
 
         NotificationMailer.join_roll_notification(user_to, user_from, roll)
       end
