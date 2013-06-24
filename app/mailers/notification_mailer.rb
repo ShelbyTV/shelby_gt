15e261286be49572a59ac65e8a30d4f771e8b84d
Henry <henry@sztul.com>
2013-06-24 16:53:30 -0400

    updating subject line of email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -147,7 +147,7 @@ class NotificationMailer < ActionMailer::Base
 
     mail :from => "Shelby.tv <#{Settings::Email.notification_sender}>",
          :to => user_to.primary_email,
-         :subject => "Weekly Recommendation Test"
+         :subject => Settings::Email.weekly_recommendation['subject']
   end
 
 end
