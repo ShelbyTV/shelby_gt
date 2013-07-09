229ede59f987f225e706fc6e29ac3c22af08bedc
Joshua Samberg <josh@shelby.tv>
2013-07-09 11:22:50 +0200

    Update subject line for weekly recommendation email

--- spec/helpers/weekly_recommendation_email_helper_spec.rb
+++ spec/helpers/weekly_recommendation_email_helper_spec.rb
@@ -2,26 +2,39 @@ require 'spec_helper'
 
 describe WeeklyRecommendationEmailHelper do
 
-  context "message_text" do
-
-    context "video graph recommendations" do
-      before(:each) do
-        @sharing_user = Factory.create(:user)
-        @src_frame = Factory.create(:frame, :creator => @sharing_user)
-        @dbe = Factory.create(:dashboard_entry, :action => DashboardEntry::ENTRY_TYPE[:video_graph_recommendation], :src_frame => @src_frame)
-      end
-
-      it "should start with the right text" do
-        message_text(@dbe).should start_with("We've discovered that people like #{@sharing_user.nickname} are")
-      end
+  context "video graph recommendations" do
+    before(:each) do
+      @sharing_user = Factory.create(:user)
+      @src_frame = Factory.create(:frame, :creator => @sharing_user)
+      @dbe = Factory.create(:dashboard_entry, :action => DashboardEntry::ENTRY_TYPE[:video_graph_recommendation], :src_frame => @src_frame)
     end
 
-    context "entertainment graph recommendations" do
-      before(:each) do
-        @friend_user = Factory.create(:user)
-        @dbe = Factory.create(:dashboard_entry, :action => DashboardEntry::ENTRY_TYPE[:entertainment_graph_recommendation])
-        @dbe.friend_likers_array = [@friend_user.id]
+    context "message_text" do
+
+      it "should return the right message" do
+        message_text(@dbe).should start_with("We've discovered that people like #{@sharing_user.nickname} are")
       end
+
+    end
+
+    context "message_subject" do
+
+      it "should return the right subject" do
+        message_subject(@dbe).should eql "Watch this video that was shared by people like #{@sharing_user.nickname}"
+      end
+
+    end
+
+  end
+
+  context "entertainment graph recommendations" do
+    before(:each) do
+      @friend_user = Factory.create(:user)
+      @dbe = Factory.create(:dashboard_entry, :action => DashboardEntry::ENTRY_TYPE[:entertainment_graph_recommendation])
+      @dbe.friend_likers_array = [@friend_user.id]
+    end
+
+    context "message_text" do
 
       it "should return the right message for one friend user" do
         message_text(@dbe, @dbe.all_associated_friends).should eql("We've discovered that #{@friend_user.nickname} checked out this video.")
@@ -48,8 +61,32 @@ describe WeeklyRecommendationEmailHelper do
         @dbe.friend_viewers_array << friend_user.id
         message_text(@dbe, @dbe.all_associated_friends).should start_with("#{@friend_user.nickname} and 2 others are")
       end
+
+    end
+
+    context "message_subject" do
+
+      it "should return the right subject for one friend user" do
+        message_subject(@dbe, @dbe.all_associated_friends).should eql("Watch this video that #{@friend_user.nickname} watched")
+      end
+
+      it "should return the right subject for two friend users" do
+        friend_user = Factory.create(:user)
+        @dbe.friend_likers_array << friend_user.id
+        message_subject(@dbe, @dbe.all_associated_friends).should eql("Watch this video that #{@friend_user.nickname} and 1 other shared, liked, and watched")
+      end
+
+      it "should return the right message for three or more friend users" do
+        (1..2).each do
+          friend_user = Factory.create(:user)
+          @dbe.friend_likers_array << friend_user.id
+        end
+        message_subject(@dbe, @dbe.all_associated_friends).should eql("Watch this video that #{@friend_user.nickname} and 2 others shared, liked, and watched")
+      end
+
     end
 
   end
+
 
 end
