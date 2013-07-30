46488f9ea9d3d543d16c764c653522e18dd1c7b1
Henry <henry@sztul.com>
2013-07-30 17:12:43 -0400

    backfilling when a user follows friends via twitter and facebook

--- lib/gt/user_facebook_manager.rb
+++ lib/gt/user_facebook_manager.rb
@@ -8,18 +8,21 @@ module GT
 
     # Find all of the user's twitter friends and follow their shelby public rolls.
     # If they're a real Shelby user, they will get an email about the following.
-    # 
+    #
     def self.follow_all_friends_public_rolls(user)
       return unless user.has_provider? "facebook"
       self.friends_ids(user).each do |uid|
         friend = User.first( :conditions => { 'authentications.provider' => 'facebook', 'authentications.uid' => uid.to_s } )
-        friend.public_roll.add_follower(user) if friend and friend.public_roll and !user.unfollowed_roll?(friend.public_roll)
+        if friend and friend.public_roll and !user.unfollowed_roll?(friend.public_roll)
+          friend.public_roll.add_follower(user)
+          GT::Framer.backfill_dashboard_entries(user, friend.public_roll, 10)
+        end
       end
     end
-    
+
     def self.verify_auth(oauth_token)
       return false unless oauth_token
-      
+
       begin
         graph = Koala::Facebook::API.new(oauth_token)
         graph.get_connections("me","permissions")
@@ -31,9 +34,9 @@ module GT
         return false
       end
     end
-    
+
     private
-    
+
       def self.friends_ids(user)
         APIClients::FacebookInfoGetter.new(user).get_friends_ids
       end
--- lib/gt/user_twitter_manager.rb
+++ lib/gt/user_twitter_manager.rb
@@ -7,21 +7,24 @@ require 'api_clients/twitter_info_getter'
 #
 module GT
   class UserTwitterManager
-    
+
     # Find all of the user's twitter friends and follow their shelby public rolls.
     # If they're a real Shelby user, they will get an email about the following.
-    # 
+    #
     def self.follow_all_friends_public_rolls(user)
       return unless user.has_provider? "twitter"
       self.friends_ids(user).each do |uid|
         friend = User.first( :conditions => { 'authentications.provider' => 'twitter', 'authentications.uid' => uid.to_s } )
-        friend.public_roll.add_follower(user) if friend and friend.public_roll and !user.unfollowed_roll?(friend.public_roll)
+        if friend and friend.public_roll and !user.unfollowed_roll?(friend.public_roll)
+          friend.public_roll.add_follower(user)
+          GT::Framer.backfill_dashboard_entries(user, friend.public_roll, 10)
+        end
       end
     end
-    
+
     def self.verify_auth(oauth_token, oauth_secret)
       return false unless oauth_token and oauth_secret
-      
+
       begin
         c = APIClients::TwitterClient.build_for_token_and_secret(oauth_token, oauth_secret)
         #this will throw if user isn't auth'd
@@ -34,12 +37,12 @@ module GT
         return false
       end
     end
-    
+
     private
-    
+
       def self.friends_ids(user)
         APIClients::TwitterInfoGetter.new(user).get_following_ids
       end
-    
+
   end
 end
