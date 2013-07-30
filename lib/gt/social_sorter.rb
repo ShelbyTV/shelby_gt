f14c77f8c065d4ae9b34f368a8935b6f3554fefc
Henry <henry@sztul.com>
2013-07-30 15:10:37 -0400

    putting loggin in for social sorter

--- lib/gt/social_sorter.rb
+++ lib/gt/social_sorter.rb
@@ -48,13 +48,16 @@ module GT
       # In the normal posting case, we make sure observing user sees it by first following that roll (unless they've specifically unfollowed it)
       # Everyone else following that public roll will see it as well.
       def self.sort_public_message(message, video_hash, observing_user, posting_user)
-
+        Rails.logger.error "[GT::SocialSorter] LOGGING (0) SocialSorter.sort_public_message called for: #{observing_user.id}"
         #observing_user should be following the posting_user's public roll, unless they specifically unfollowed it
         unless posting_user.public_roll.followed_by?(observing_user) or observing_user.unfollowed_roll?(posting_user.public_roll)
+          Rails.logger.error "[GT::SocialSorter] LOGGING (1) user: #{observing_user.id} about to follow #{posting_user.id}"
           posting_user.public_roll.add_follower(observing_user, false)
           # To make sure new users get a bunch of historical content, backfill.
           # (This means old users will also get the backfill when they follow sombody new on twitter/fb)
+          Rails.logger.error "[GT::SocialSorter] LOGGING (2) about to backfill dashboard_entries for: #{observing_user.id}"
           GT::Framer.backfill_dashboard_entries(observing_user, posting_user.public_roll, 20)
+          Rails.logger.error "[GT::SocialSorter] LOGGING (3) done backfilling dashboard_entries for: #{observing_user.id}"
           new_following = true
         end
 
@@ -64,6 +67,7 @@ module GT
           unless old_frame.conversation and old_frame.conversation.messages.any? { |m| m.origin_id == message.origin_id }
             # This message hasn't been appended to the conversation yet
             # Do so atomically (w/ set semantics so timing doesn't result in multiple posts)
+            Rails.logger.error "[GT::SocialSorter] LOGGING (3) about  add a conversation to set for: #{observing_user.id}"
             Conversation.add_to_set(old_frame.conversation.id, :messages => message.to_mongo)
           end
         else
@@ -80,6 +84,7 @@ module GT
         end
 
         if !res
+          Rails.logger.error "[GT::SocialSorter] LOGGING (4) no result frame for: #{observing_user.id}"
           # New frame was not created b/c 1) conversation was already posted OR 2) video was posted recently and we added this message to it
           #  BUT if observing_user was just added as a follower of posting_user's public_roll,
           #      a DashboardEntry may not have been created for this Frame/observing_user...
