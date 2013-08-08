4a98afd5a5c4c67a29d57bf25294f3fd9edcb227
Joshua Samberg <josh@shelby.tv>
2013-08-08 13:30:30 -0400

    Mortar email formatted and showing correct number of items

--- app/mailers/mortar_mailer.rb
+++ app/mailers/mortar_mailer.rb
@@ -1,9 +1,10 @@
 class MortarMailer < ActionMailer::Base
 
-  def mortar_recommendation_trial(user_to, recs)
+  def mortar_recommendation_trial(user_to, recs, include_reason=false)
 
     @user = user_to
     @recs = recs
+    @include_reason = include_reason
 
     mail :from => "Shelby.tv Mortar Trial <dev@shelby.tv>",
       :to => user_to.primary_email,
--- lib/utils/mortar_harvester.rb
+++ lib/utils/mortar_harvester.rb
@@ -5,9 +5,9 @@ module GT
     # pass a user object or user id as the parameter
     def self.get_recs_for_user(u)
       user_id = u.is_a?(User) ? u.id : u
-      response = HTTParty.get("https://recs.mortardata.com/v1/recommend/users/#{user_id}",
+      response = HTTParty.get("https://recs.mortardata.com/v1/recommend/users/#{user_id}?limit=9",
                      :basic_auth => {:username => 'henry@shelby.tv', :password => 'review-before-policeman-she'})
-      response.code == 200 ? response : nil
+      response.code == 200 ? response.parsed_response['recommended_items'] : nil
     end
   end
 end
