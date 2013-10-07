a74bbcab0adf22758c163405d0f2bb75552a275f
Joshua Samberg <josh@shelby.tv>
2013-10-07 17:43:41 -0400

    Allow deploy:current_branch cap task to deploy other branches when deploying to email;DOESN'T WORK FOR PRODUCTION FOR SAFETY

--- config/deploy/email.rb
+++ config/deploy/email.rb
@@ -16,3 +16,11 @@ set :scm, :git
 set :repository,  "git@github.com:ShelbyTV/shelby_gt.git"
 set :branch, "master"
 set :deploy_via, :remote_cache #keep a local cache to speed up deploys
+
+namespace :deploy do
+  desc "Deploy the currently checked out branch"
+  task :current_branch do
+    set :branch, `git rev-parse --abbrev-ref HEAD`.rstrip
+    deploy.default
+  end
+end
