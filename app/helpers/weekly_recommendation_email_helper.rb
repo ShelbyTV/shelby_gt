02a2f652256568bfde61f5d9b9891b622fb73d13
Michael Matyus <michael@maty.us>
2013-10-08 12:14:10 -0400

    organizing the shit out of the videocard for email recommendations

--- app/helpers/weekly_recommendation_email_helper.rb
+++ app/helpers/weekly_recommendation_email_helper.rb
@@ -28,4 +28,46 @@ module WeeklyRecommendationEmailHelper
     end
   end
 
+  def frame_footer_message(dbe)
+    if dbe.action == DashboardEntry::ENTRY_TYPE[:video_graph_recommendation]
+      # VIDEO GRAPH
+      if dbe.src_frame && dbe.src_frame.creator
+        return "This video is similar to videos #{dbe.src_frame.creator.nickname} has shared"
+      else
+        return "Check out this video"
+      end
+
+    elsif dbe.action == DashboardEntry::ENTRY_TYPE[:entertainment_graph_recommendation]
+      return "There are a lot of people are sharing, liking, and watching this video"
+
+    elsif dbe.action == DashboardEntry::ENTRY_TYPE[:mortar_recommendation]
+      # MORTAR GRAPH
+      if dbe.src_video && dbe.src_video.title
+        return "Because you shared #{dbe.src_video.title}"
+      else
+        return "This video is similar to videos you have shared"
+      end
+
+    elsif dbe.action == DashboardEntry::ENTRY_TYPE[:channel_recommendation]
+      # CHANNEL
+      return dbe.frame.conversation.messages.first.text if dbe.frame.conversation and dbe.frame.conversation.messages and dbe.frame.conversation.messages.first
+    else
+      return dbe.frame.conversation.messages.first.text if dbe.frame.conversation and dbe.frame.conversation.messages and dbe.frame.conversation.messages.first
+    end
+  end
+
+  def frame_footer_avatar(dbe)
+    if dbe.action == DashboardEntry::ENTRY_TYPE[:video_graph_recommendation]
+      return "//#{Settings::Global.web_host}/images/recommendations/share-2.jpg"
+    elsif dbe.action == DashboardEntry::ENTRY_TYPE[:entertainment_graph_recommendation]
+      return "//#{Settings::Global.web_host}/images/recommendations/share-1.jpg"
+    elsif dbe.action == DashboardEntry::ENTRY_TYPE[:mortar_recommendation]
+      return "//#{Settings::Global.web_host}/images/recommendations/share-2.jpg"
+    elsif dbe.action == DashboardEntry::ENTRY_TYPE[:channel_recommendation]
+      return avatar_url_for_user(dbe.frame.creator)
+    else
+      return avatar_url_for_user(dbe.frame.creator)
+    end
+  end
+
 end
