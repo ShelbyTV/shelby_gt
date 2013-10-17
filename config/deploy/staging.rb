5b498a4e40cd819b1e21a1e91bbf190809bf33be
Joshua Samberg <josh@shelby.tv>
2013-10-17 10:24:29 -0400

    Deployments to staging deploy from the staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,18 +15,10 @@ role :app, "166.78.255.147"
 #############################################################
 
 set :repository,  "git@github.com:ShelbyTV/shelby_gt.git"
-set :branch, fetch(:branch, "master")
+set :branch, "staging"
 
 set :rails_env, "staging"
 set :unicorn_env, "staging"
 set :app_env,     "staging"
 
 after 'deploy:restart', 'unicorn:duplicate'
-
-namespace :deploy do
-  desc "Deploy the currently checked out branch"
-  task :current_branch do
-    set :branch, `git rev-parse --abbrev-ref HEAD`.rstrip
-    deploy.default
-  end
-end
