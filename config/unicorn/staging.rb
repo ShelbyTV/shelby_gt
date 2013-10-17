9d219316f86ac1c51af8f0abd2fda0f448c3a598
Joshua Samberg <josh@shelby.tv>
2013-10-17 15:02:37 -0400

    Increase the unicorn timeout just in case that will allow OAuth redirection enough time to complete

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -4,7 +4,7 @@ app_path = "/home/gt/api/current"
 # Set unicorn options
 worker_processes 4
 preload_app true
-timeout 30
+timeout 60
 listen "/tmp/shelby-gt-api.socket", :backlog => 64
 #listen 8080, :tcp_nopush => true
 
