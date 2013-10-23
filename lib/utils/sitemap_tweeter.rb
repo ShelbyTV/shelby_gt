dbf79745590a705cb5aefe602b977c045b821b9a
Henry <henry@sztul.com>
2013-10-23 14:21:15 -0400

    creating module to tweet videos from a sitemap on behalf of a user

---
+++ lib/utils/sitemap_tweeter.rb
@@ -0,0 +1,52 @@
+require "twitter_client"
+require "nokogiri"
+require "zlib"
+
+module Dev
+  class SitemapTweeter < TwitterClient
+
+    def initialize(user, sitemap_number, options)
+      @sitemap_number = sitemap_number
+      @user = user #off shelby is: 4e55654cf6db241c220003c2
+      @limit = options.limit if options.has_key? :limit
+    end
+
+    def tweet_urls
+      setup_for_user(@user)
+      sitemap_file_location = "/home/gt/api/current/public/system/sitemap#{@sitemap_number}.xml.gz"
+      i = 0
+      begin
+        Zlib::GzipReader.open(sitemap_file_location) { |gz|
+          @f = gz.read
+        }
+        doc = Nokogiri::XML(@f)
+      rescue => e
+        Rails.logger.error "[SitemapTweeter] Error loading xml into Nokogiri. #{e}"
+      end
+
+      doc.css('urlset url').each do |n|
+        return if @limit and (i > @limit)
+        url = n.child.text
+        url_frag = url.split('/')
+        video_provider = url_frag[4]
+        video_id = url_frag[5]
+        # get video from shelby
+        if video_provider and video_id
+          shelby_video = Video.first(:provider_name => video_provider, :provider_id => video_id)
+          video_permalink = shelby_video.video_permalink
+          video_title = shelby_video.title
+          # tweet from offshelby about video
+          tweet_text = video_title + " ▶ " + video_permalink
+          begin
+            tweet = twitter_client.statuses.update! :status => tweet_text
+            puts tweet_text if tweet
+          rescue => e
+            Rails.logger.error "[SitemapTweeter] Error posting to Twitter. #{e}"
+          end
+        end
+        i += 1
+      end
+    end
+
+  end
+end
