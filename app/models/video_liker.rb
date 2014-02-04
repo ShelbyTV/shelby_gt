0791744b45d1f8dfbcf2f1243ee5a0adb7c4f26e
Joshua Samberg <josh@shelby.tv>
2014-02-04 18:26:52 -0500

    Protect ourselves from user_ids that we can't find in the db (though I don't know how those woudl get in here)

--- app/models/video_liker.rb
+++ app/models/video_liker.rb
@@ -17,12 +17,12 @@ class VideoLiker
 
   # lookup the user who liked the video and update the denormalized user data stored on this document
   def refresh_user_data!
-    user = User.fields(:name, :nickname, :user_image, :user_image_original, :avatar_file_name).first(:id => self.user_id)
-
-    self.name = user.name
-    self.nickname = user.nickname
-    self.user_image = user.user_image
-    self.user_image_original = user.user_image_original
-    self.has_shelby_avatar = user.has_shelby_avatar
+    if user = User.fields(:name, :nickname, :user_image, :user_image_original, :avatar_file_name).first(:id => self.user_id)
+      self.name = user.name
+      self.nickname = user.nickname
+      self.user_image = user.user_image
+      self.user_image_original = user.user_image_original
+      self.has_shelby_avatar = user.has_shelby_avatar
+    end
   end
 end
